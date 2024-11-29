fad5485243b1e4f18fc516faa658b73b075c9120
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-11-29 15:22:51 +0100

    feat: add emoji, h1 and indicate how multiple it is

--- script.js
+++ script.js
@@ -36,11 +36,11 @@ EDIT: } else {console.log (i)}
     
 for (let i = 1; i <= 100; i++ ) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log ("FizzBuzz")
+        console.log ("🔥 FizzBuzz 🔥 - multiple of 3 and 5")
     } else if (i % 3 === 0) {
-        console.log ("Fizz")
+        console.log ("🔥 Fizz - multiple of 3")
     } else if (i % 5 === 0) {
-        console.log ("Buzz")
+        console.log ("Buzz 🔥 - multiple of 5")
     } else {
         console.log (i)
     }

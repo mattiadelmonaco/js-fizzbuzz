7dc15c45382e63bca4b79001cea5ecfcd822a412
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-11-29 14:55:04 +0100

    feat: add problem's steps

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ Abbiamo visto qualcosa di particolare che possiamo usare?
 Consigli del giorno:
 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+STEPS: 
+
+1. Creo un contatore che va da 1 fino a 100
+2. SE il numero è un multiplo di 3 devo stampare "Fizz"
+    - per sapere se è multiplo devo indicare che il resto (modulo - %) del numero deve essere uguale a 0
+3. ALTRIMENTI SE il numero è un multiplo di 5 devo stampare "Buzz"
+    - per sapere se è multiplo devo indicare che il resto (modulo - %) del numero deve essere uguale a 0
+4. ALTRIMENTI SE il numero è multiplo sia di 3 che di 5 devo stampare "FizzBuzz"
+    - per sapere se è multiplo di entrambi devo usare l'operatore logico AND (&&) così da indicare che entrambi i casi 
+    devono verificarsi per stampare "FizzBuzz" in console (deve verificarsi resto 0 a multiplo di 3 e anche a multiplo di 5)
+5. ALTRIMENTI se non si verifica nessuna delle precedenti condizioni stampo direttamente il numero in console
 */
